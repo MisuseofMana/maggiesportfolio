e1024136fc3aa703c5c488e182d0590db2b136ad
Sean Yager <contact@seanyager.com>
2022-06-22 09:41:43 -0400

    intial commit, scaffold layout

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app dark>
+    <v-container>
+      <v-toolbar flat :extended="false">
+        <v-row align="center">
+          <v-col cols="4">
+            <v-toolbar-title>Maggie Yager</v-toolbar-title>
+          </v-col>
+          <v-col cols="8">
+            <v-tabs right>
+              <v-tab>Something</v-tab>
+              <v-tab>Something Else</v-tab>
+            </v-tabs>
+          </v-col>
+        </v-row>
+      </v-toolbar>
+      <v-main>
+        <Nuxt />
+      </v-main>
+      <v-footer color="white"> Footer </v-footer>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
